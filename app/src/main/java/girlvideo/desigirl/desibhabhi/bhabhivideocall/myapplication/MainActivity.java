210ebce75ac09df511fb10d8bbb535f5cf440a55
Udit1049 <uditpatel194@gmail.com>
2021-11-10 16:56:32 +0530

    Initial commit

---
+++ app/src/main/java/girlvideo/desigirl/desibhabhi/bhabhivideocall/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package girlvideo.desigirl.desibhabhi.bhabhivideocall.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
